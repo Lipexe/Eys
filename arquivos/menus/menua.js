26e0f08cd2f53c0181079542e137902c670fa000
Lipexe <127744724+Lipexe@users.noreply.github.com>
2023-05-20 12:21:57 -0300

    Add files via upload

---
+++ arquivos/menus/menua.js
@@ -0,0 +1,207 @@
+const menu = (prefix) => {
+  
+return `
+                      𝙔𝙪𝙯𝙪𝘽𝙊𝙏
+      ━━━━━━━━❯✷❮━━━━━━━━ 
+      
+┏━━━━━━━━━━━━
+║〆  𝙋𝙧𝙚𝙛𝙞𝙭: 「 ${prefix} 」
+║〆  𝙎𝙩𝙖𝙩𝙪𝙨 : Online
+║〆  𝘽𝙮: 
+║〆  𝙏𝙤𝙩𝙖𝙡 𝙘𝙢𝙙:
+┗━━━━━━━━━━━━ 
+
+┏━━「 𝙐𝙨𝙚𝙧 」 ━━━
+║〆  nome:  ${"\u200B".repeat(4000)} 
+║〆  Level: 
+║〆  XP: 
+║〆  Patente: 
+┗━━━━━━━━━━━━
+
+┏━━「 𝙋𝙤𝙥𝙪𝙡𝙖𝙧𝙚𝙨 」 ━━━
+║〆  *${prefix}sticker* (img) 
+┃ > fig marcando a imagem
+║〆  *${prefix}play* (msc) 
+┃ > baixa a música escolhida 
+║〆  *${prefix}toimg* (fig) 
+┃ > transforma a fig em img
+║〆  *${prefix}attp* (txt) 
+┃ > faz fig com o texto 
+║〆  *${prefix}ativos* 
+┃ > mostra os ativos do bot             
+┗━━━━━━━━━━━━━━━━━
+
+┏━━「 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 𝙣𝙤𝙬 」 ━━━
+║〆  *${prefix}play* 
+┃ > 
+║〆  *${prefix}ttd* 
+┃ > 
+║〆  *${prefix}instadw* 
+┃ > 
+║〆  *${prefix}ytsearch* 
+┃ > 
+║〆  *${prefix}html* 
+┃ > 
+║〆  *${prefix}playstore* 
+┃ > 
+║〆  *${prefix}videourl* 
+┃ > 
+║〆  *${prefix}imgurl* 
+┃ > 
+┗━━━━━━━━━━━━━━━━━
+
+┏━━「 𝙂𝙧𝙤𝙪𝙥𝙨 」 ━━━
+║〆  *${prefix}nomegp* 
+┃ > 
+║〆  *${prefix}setfotogp* 
+┃ > 
+║〆  *${prefix}promover* 
+┃ > 
+║〆  *${prefix}demote* 
+┃ > 
+║〆  *${prefix}setdesc* 
+┃ > 
+║〆  *${prefix}infogp* 
+┃ > 
+║〆  *${prefix}reviver* 
+┃ > 
+║〆  *${prefix}status* 
+┃ > 
+║〆  *${prefix}leveling* 
+┃ > 
+║〆  *${prefix}level* 
+┃ > 
+║〆  *${prefix}atividade* 
+┃ > 
+║〆  *${prefix}Simih* 
+┃ > 
+║〆  *${prefix}bemvindo* 
+┃ > 
+║〆  *${prefix}lbv* 
+┃ > 
+║〆  *${prefix}lsaiu* 
+┃ > 
+║〆  *${prefix}grupo* a/f
+┃ > 
+║〆  *${prefix}linkgp* 
+┃ > 
+║〆  *${prefix}totag* 
+┃ > 
+║〆  *${prefix}hidetag* 
+┃ > 
+║〆  *${prefix}kick* 
+┃ > 
+║〆  *${prefix}add* 
+┃ > 
+║〆  *${prefix}bcgp* 
+┃ > 
+║〆  *${prefix}banghost* 
+┃ > 
+║〆  *${prefix}bug* 
+┃ > 
+║〆  *${prefix}sugestao* 
+┃ > 
+║〆  *${prefix}listonline* 
+┃ > 
+║〆  *${prefix}admins* 
+┃ > 
+┗━━━━━━━━━━━━━━━━━
+
+┏━━「  」 ━━━
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+┗━━━━━━━━━━━━━━━━━
+
+┏━━「 𝘿𝙚𝙛𝙚𝙣𝙨𝙚 」 ━━━
+║〆  *${prefix}antilink* 
+┃ > 
+║〆  *${prefix}antiimg* 
+┃ > 
+║〆  *${prefix}antivideo* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+┗━━━━━━━━━━━━━━━━━
+
+┏━━「 𝙁𝙪𝙣 𝙖𝙣𝙙 𝙟𝙤𝙮 」 ━━━
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+┗━━━━━━━━━━━━━━━━━
+
+┏━━「  」 ━━━
+║〆  *${prefix}idiomas* 
+┃ > mostra os idiomas do gtts
+║〆  *${prefix}destrava* 
+┃ > 
+║〆  *${prefix}destrava2* 
+┃ > 
+║〆  *${prefix}ocr* 
+┃ > 
+║〆  *${prefix}listpremium* 
+┃ > 
+║〆  *${prefix}ddd* 
+┃ > 
+║〆  *${prefix}encurtalink* 
+┃ > 
+║〆  *${prefix}gerarcpf* 
+┃ > 
+║〆  *${prefix}figuid* 
+┃ > 
+║〆  *${prefix}get* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+┗━━━━━━━━━━━━━━━━━
+
+┏━━「 𝙁𝙤𝙧 𝙤𝙬𝙣𝙚𝙧𝙨 」 ━━━
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+║〆  *${prefix}* 
+┃ > 
+┗━━━━━━━━━━━━━━━━━
+
+`
+}
+
+exports.menu = menu
+
